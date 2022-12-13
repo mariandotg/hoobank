1b0e782e745614cd2c74001a831e051e9121ea4d
mariandotg <marianguillaume.m@gmail.com>
2022-12-13 13:07:18 -0300

    Added base contentful service

---
+++ services/contentful.ts
@@ -0,0 +1,21 @@
+const contentfulSDK = require('contentful')
+
+const client = contentfulSDK.createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+})
+
+const getEntries = async (contentType: string) => {
+  const entries = await client.getEntries({
+    content_type: contentType,
+    include: 1,
+  })
+
+  return entries
+}
+
+const contentful = {
+  getEntries,
+}
+
+export default contentful
